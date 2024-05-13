3e471c4284c09bdc7e1ec9b875665b5a469a182d
BY0808 <byahn.13@gmail.com>
2024-05-13 14:29:14 +0900

    카메라 따라가기 구현
    카메라가 타겟(캐릭터) 따라서 화면 보여줌

---
+++ Assets/Scripts/Entities/Controllers/CameraViewController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewController : MonoBehaviour
+{
+    //카메라가 따라다닐 타겟
+    public GameObject Target;
+
+    //연결할 카메라 불러오기
+    private Camera cameraView;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        cameraView = GetComponent<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        ViewMovement();
+    }
+
+    private void ViewMovement()
+    {
+        //타겟 위치 계산
+        Vector3 moveDirection = new Vector3(Target.transform.position.x, Target.transform.position.y, cameraView.transform.position.z);
+        //타겟 위치를 카메라 위치에 넣어줌
+        cameraView.transform.position = moveDirection;
+    }
+}
