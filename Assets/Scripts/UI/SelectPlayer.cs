dcae11cf94c7c82bba5b90efa65ffa16c6abe9ef
BY0808 <byahn.13@gmail.com>
2024-05-13 21:13:48 +0900

    캐릭터 프리팹화

--- Assets/Scripts/UI/SelectPlayer.cs
+++ Assets/Scripts/UI/SelectPlayer.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class SelectPlayer : MonoBehaviour
 {
     public CharacterList character;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    public SelectPlayer[] selectPlayers;
+
+    private void Awake()
+    {
+        anim = GetComponent<SpriteRenderer>().GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     //마우스 버튼을 눌렀다가 떼었을 때만 호출 메소드
-    private void OnMouseDown()
+    private void OnMouseUpAsButton()
     {
         SelectResult();
+        OnSelect("isWalking");
     }
 
     private void SelectResult()
     {
         //선택한 캐릭터를 PlayerData에 저장
         PlayerData.instance.currentCharacter = character;
+    }
+
+    private void OnSelect(string boolName)
+    {
+        anim.SetBool(boolName, true);
+        spriteRenderer.color = new Color(1f,1f, 1f);
     }
 }
